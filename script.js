fd56e3810a499f14b620bd217b7853ea31336a96
Elpetra <k158vx@yandex.ru>
2024-02-05 13:51:54 +0300

    changing images added

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let drawNode = document.querySelector(".draw");
 let btnsNode = document.querySelector(".btns");
 let btnNext = document.querySelector(".next");
 let btnRestart = document.querySelector(".restart");
-let leftHandNode = document.querySelector(".left__hand");
 let countAi = 0;
 let countHuman = 0;
 
@@ -22,7 +21,7 @@ for (let el of btnNode) {
 function compare(choise) {
   let arr = ["rock", "paper", "scissors"];
   let randomN = Math.floor(Math.random() * arr.length);
-  console.log(arr[randomN]);
+    console.log(arr[randomN]);
   if (arr[randomN] === choise) {
     console.log("IT'S  A  DRAW");
     countAi += 1;
@@ -35,11 +34,9 @@ function compare(choise) {
     console.log("выиграл человек");
     countHuman += 1;
   } else if (arr[randomN] === "paper" && choise === "scissors") {
-    //   document.getElementsByClass("left__hand").src = "./src/images/paper-ai";
     console.log("выиграл человек");
     countHuman += 1;
   } else if (arr[randomN] === "scissors" && choise === "rock") {
-    //   document.getElementsByClass("left__hand").src = "./src/images/scissors-ai";
     console.log("выиграл человек");
     countHuman += 1;
   } else {
@@ -54,6 +51,22 @@ function compare(choise) {
   scoreAi.textContent = countAi;
   scoreHuman.textContent = countHuman;
 }
+
+
+// не понимаю куда надо засунуть содержимое этой функции или вызов этой функции и картинки в любом случае меняются не корректно
+// function changeImg() {
+//     if (arr[randomN] === arr[1]) {
+// document.getElementById("left-hand").src = "./src/images/AI Arm_Paper";
+//     } else if (arr[randomN] === arr[2]) {
+// document.getElementById("left-hand").src = "./src/images/AI Arm_Scissors";
+//     } else if (choise === "paper") {
+// document.getElementById("right-hand").src = "./src/images/Human Arm_Paper";       
+//     } else if (choise === "scissors") {
+// document.getElementById("right-hand").src = "./src/images/Human Arm_Scissors";
+//     }
+// }
+
+
 
 btnNext.addEventListener("click", nextGame);
 btnRestart.addEventListener("click", restartGame);
